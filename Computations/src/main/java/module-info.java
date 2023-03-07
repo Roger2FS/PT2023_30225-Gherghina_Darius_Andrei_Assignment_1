0fe8cef9b8fb73e5260665043262c75b4732c241
Asus <darius_gherghina@yahoo.ro>
2023-03-07 13:35:04 +0200

    2

--- Computations/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.computations {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.computations to javafx.fxml;
-    exports com.example.computations;
-}
